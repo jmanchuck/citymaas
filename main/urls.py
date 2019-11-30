7a671f06ab7eca3c261598547a06d0d16cfdb6a1
chai41104@hotmail.com <thongchai.wirojsakseree@blueprism.com>
2019-11-30 23:23:11 +0000

    add CityMaas/wsgi.py

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
-    path('map/', views.map, name='map')
+    path('map/', views.map, name='map'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact')
 ]

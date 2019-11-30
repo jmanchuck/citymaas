856eb7c9a6859a2bd45583acc5f44ff415f4b612
jmanchuck <jjcheung0000@gmail.com>
2019-11-30 16:39:43 +0000

    Working static

---
+++ main/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.utils import timezone
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from urllib.parse import urlencode
+
+# Create your views here.
+
+def main(request):
+    if request.method == "GET":
+        return render(request, 'main/index.html')
+
+    elif request.method == "POST":
+        # do back end here
+        pass
+
+def destination(request):
+    if request.method == "GET":
+        return render(request, 'main/index.html')
+
+    elif request.method == "POST":
+        return
